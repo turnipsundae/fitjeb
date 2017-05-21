2e02228c92ea65799491f8f1d9c0e2eb53748fb3
Kevin Lau <laudevelopment@gmail.com>
2017-05-21 01:57:39 -0700

    Fix createsu command

--- browse/management/commands/createsu.py
+++ browse/management/commands/createsu.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-import os.environ as env
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if 'ADMIN_USERNAME' in env and 'ADMIN_PASSWORD' in env and 'ADMIN_EMAIL' in env:
-            if not User.objects.filter(username=env['ADMIN_USERNAME']).exists():
-                User.objects.create_superuser(env['ADMIN_USERNAME'], env['ADMIN_PASSWORD'], env['ADMIN_EMAIL'])
+        if 'ADMIN_USERNAME' in os.environ and 'ADMIN_PASSWORD' in os.environ and 'ADMIN_EMAIL' in os.environ:
+            if not User.objects.filter(username=os.environ['ADMIN_USERNAME']).exists():
+                User.objects.create_superuser(os.environ['ADMIN_USERNAME'], os.environ['ADMIN_PASSWORD'], os.environ['ADMIN_EMAIL'])
