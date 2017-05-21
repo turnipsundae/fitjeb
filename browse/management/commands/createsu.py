00729349a64c4776cdc87c0a2d46f2875c6fcfbd
Kevin Lau <laudevelopment@gmail.com>
2017-05-21 02:11:42 -0700

    Test hard coded admin

--- browse/management/commands/createsu.py
+++ browse/management/commands/createsu.py
@@ -6,6 +6,5 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if 'ADMIN_USERNAME' in os.environ and 'ADMIN_PASSWORD' in os.environ and 'ADMIN_EMAIL' in os.environ:
-            if not User.objects.filter(username=os.environ['ADMIN_USERNAME']).exists():
-                User.objects.create_superuser(os.environ['ADMIN_USERNAME'], os.environ['ADMIN_PASSWORD'], os.environ['ADMIN_EMAIL'])
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "crunchylaua@gmail.com", "admin")
