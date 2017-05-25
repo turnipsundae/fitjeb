9151951846e84a6ced2624a90cd09a46f22c1e2c
Kevin Lau <laudevelopment@gmail.com>
2017-05-24 21:27:19 -0700

    Add crawler tasks

---
+++ crawl/settings.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+CROSSFIT_WOD_URL = "https://www.crossfit.com/workout"
+CRAWLER_START_DATE = datetime(2017, 1, 1)
+CRAWLER_FREQ_MINUTE = "*/1"
+CRAWLER_FREQ_HOUR = "0"
---
+++ crawl/tasks.py
@@ -0,0 +1,30 @@
+from celery.task.schedules import crontab
+from celery.decorators import task, periodic_task
+from celery.utils.log import get_task_logger
+
+from crawl.utils import save_latest_wod, save_old_wod
+
+from crawl.settings import CRAWLER_FREQ_MINUTE, CRAWLER_START_DATE
+
+logger = get_task_logger(__name__)
+
+@task(name="task_save_latest_wod")
+def task_save_latest_wod():
+    """
+    Saves latest wod from Crossfit.com
+    """
+    save_latest_wod()
+    logger.info("Saved WOD from Crossfit.com")
+
+@periodic_task(
+    run_every=(crontab(minute=CRAWLER_FREQ_MINUTE)),
+    name="task_save_specific_wod",
+    ignore_result=True
+)
+def task_save_specific_wod():
+    """
+    Saves WOD from Crossfit.com beginning from start date,
+    and skipping past any previously crawled dates.
+    """
+    save_old_wod(CRAWLER_START_DATE)
+    logger.info("Saved WOD from Crossfit.com")
