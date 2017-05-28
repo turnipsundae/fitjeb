e7772b87bb9a0b33ca8fca7cb9de2a232046bec7
Kevin Lau <laudevelopment@gmail.com>
2017-05-28 15:06:49 -0700

    Test celerybeat

--- crawl/tasks.py
+++ crawl/tasks.py
@@ -4,7 +4,7 @@ from celery.utils.log import get_task_logger
 
 from crawl.utils import save_latest_wod, save_old_wod
 
-from crawl.settings import CRAWLER_FREQ_MINUTE, CRAWLER_START_DATE
+from crawl.settings import CRAWLER_FREQ_MINUTE, CRAWLER_FREQ_HOUR, CRAWLER_START_DATE
 
 logger = get_task_logger(__name__)
 
@@ -13,11 +13,11 @@ def task_save_latest_wod():
     """
     Saves latest wod from Crossfit.com
     """
-    save_latest_wod()
+    # save_latest_wod()
     logger.info("Saved WOD from Crossfit.com")
 
 @periodic_task(
-    run_every=(crontab(minute=CRAWLER_FREQ_MINUTE)),
+    run_every=(crontab(minute=CRAWLER_FREQ_MINUTE, hour=CRAWLER_FREQ_HOUR)),
     name="task_save_specific_wod",
     ignore_result=True
 )
@@ -26,5 +26,5 @@ def task_save_specific_wod():
     Saves WOD from Crossfit.com beginning from start date,
     and skipping past any previously crawled dates.
     """
-    save_old_wod(CRAWLER_START_DATE)
+    # save_old_wod(CRAWLER_START_DATE)
     logger.info("Saved WOD from Crossfit.com")
