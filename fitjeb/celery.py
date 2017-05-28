a0be787ba7990594c9e16c84bbadeab3b7a19319
Kevin Lau <laudevelopment@gmail.com>
2017-05-28 10:13:06 -0700

    Remove manual logging from celery.py

--- fitjeb/celery.py
+++ fitjeb/celery.py
@@ -8,17 +8,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitjeb.settings.base')
 import urllib
 
 if 'AWS_ACCESS_KEY_ID' in os.environ and 'AWS_SECRET_ACCESS_KEY' in os.environ:
-
     app = Celery('fitjeb', broker='sqs://{0}:{1}@'.format(
         urllib.parse.quote(os.environ['AWS_ACCESS_KEY_ID'], safe=''),
         urllib.parse.quote(os.environ['AWS_SECRET_ACCESS_KEY'], safe='')
     ))
-    with open('logfile', 'w') as f:
-        f.write('Success')
 else:
     app = Celery('fitjeb')
-    with open('logfile', 'w') as f:
-        f.write('AWS access keys not in environment variables')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
