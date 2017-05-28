a00a2abf1517181a3faab8e78338ed731b6e3d69
Kevin Lau <laudevelopment@gmail.com>
2017-05-27 20:40:25 -0700

    Change celery config settings

--- fitjeb/celery.py
+++ fitjeb/celery.py
@@ -5,15 +5,14 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitjeb.settings.base')
 
-# app = Celery('fitjeb', broker="sqs://AKIAILCB5LDQT5PORQWQ:+eny2pQ5sqGZqOSPwHLEDevmaza6sd47K2zbCc7D@")
 app = Celery('fitjeb')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.config_from_object('fitjeb.settings.base', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('fitjeb.settings.prod', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
