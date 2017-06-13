d5f4610edc78d9fa2c067fb8a0a6a4d7fb4edb96
Kevin Lau <laudevelopment@gmail.com>
2017-06-12 19:41:02 -0700

    Hello world

--- fitjeb/urls.py
+++ fitjeb/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url(r'^$', views.hello, name='hello'),
     url(r'^admin/', admin.site.urls),
 ]
