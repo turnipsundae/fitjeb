5c596d09804fbb955726dd5a0df638f6fca32e17
Kevin Lau <laudevelopment@gmail.com>
2017-06-21 19:09:56 -0700

    Add browse

---
+++ browse/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'browse'
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.WorkoutDetailView.as_view(), name='workoutdetail'),
+
+]
