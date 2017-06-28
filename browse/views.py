a8e881c31257d8b88b119bef5e1a75e0f7b1bff5
Kevin Lau <laudevelopment@gmail.com>
2017-06-27 21:52:06 -0700

    Add paginator

--- browse/views.py
+++ browse/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
@@ -7,8 +8,11 @@ from crawl.models import Workout
 # Create your views here.
 
 class IndexView(generic.ListView):
-    model = Workout
+    # queryset = Workout.objects.all() is the long form of model = Workout
+    queryset = Workout.objects.all().order_by('-date')
     template_name = 'browse/index.html'
+    context_object_name = 'workout_list'
+    paginate_by = 8
 
 class WorkoutDetailView(generic.DetailView):
     model = Workout
@@ -26,6 +30,9 @@ class WorkoutDetailView(generic.DetailView):
                 gender.min_score = "{:.0f}".format(gender.min_score) 
                 gender.avg_score = "{:.0f}".format(gender.avg_score)
                 gender.max_score = "{:.0f}".format(gender.max_score)
+        # Make gender readable
+        gender_key = {"F":"Female","M":"Male","E":"Not Specified"}
+        for gender in benchmark_list: gender.gender = gender_key[gender.gender]
         return benchmark_list
 
     def get_context_data(self, **kwargs):
@@ -36,12 +43,3 @@ class WorkoutDetailView(generic.DetailView):
         # Change format of results for better readability
         context['benchmark_list'] = self.get_pretty_results(context['workout'].uom, context['benchmark_list'])
         return context
-
-from crawl.models import Workout
-
-def CeleryView(request):
-    result = Workout.objects.count()
-    context = {
-        "result" : result,
-    }
-    return render(request, 'browse/celery.html', context)
